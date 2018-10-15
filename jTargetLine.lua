7b5ff123e107a5e496a36cc1049dbf4a59361d2d
Jean Alexander Woldner <alexander@woldner.io>
2018-10-15 16:50:47 +0200

    add class colors

--- jTargetLine.lua
+++ jTargetLine.lua
@@ -1,14 +1,19 @@
 local AddonName, Addon = ...
 
+local select = select
+local unpack = unpack
+
 local _G = _G
 local CreateFrame = _G.CreateFrame
-local C_NamePlate = _G.C_NamePlate
-local GetNamePlateForUnit = C_NamePlate.GetNamePlateForUnit
+local UnitClass = _G.UnitClass
+local UnitIsPlayer = _G.UnitIsPlayer
 local issecure = _G.issecure
+local GetNamePlateForUnit = _G.C_NamePlate.GetNamePlateForUnit
 
 local UNIT_TARGET = "target"
 local LINE_THICKNESS = 3
 local LINE_OFFSET = -25
+local DEFAULT_LINE_COLOR = {.5, .5, .5, 1}
 
 function Addon:ShowLine()
   if (not self.line:IsVisible()) then
@@ -30,22 +35,18 @@ function Addon:ToggleLine(visible)
   end
 end
 
-function Addon:GetLastUnit()
-  return self:GetLastNameplate().namePlateUnitToken or nil
+function Addon:GetNamePlateUnit()
+  return self:GetNamePlate().namePlateUnitToken or nil
 end
 
-function Addon:GetLastNameplate()
-  return self.lastNameplate or {}
+function Addon:GetNamePlate()
+  return self.namePlate or {}
 end
 
-function Addon:AnchorLine(nameplate)
-  local lastNameplate = self:GetLastNameplate()
-
-  if (nameplate and (nameplate ~= lastNameplate)) then
+function Addon:AnchorLine(namePlate)
+  if (namePlate and (self:GetNamePlate() ~= namePlate)) then
     self.line:SetStartPoint("CENTER", self.frame)
-    self.line:SetEndPoint("CENTER", nameplate, 0, LINE_OFFSET)
-
-    self.lastNameplate = nameplate
+    self.line:SetEndPoint("CENTER", namePlate, 0, LINE_OFFSET)
   end
 end
 
@@ -53,7 +54,7 @@ function Addon:SetupLine()
   self.line = self.frame:CreateLine()
 
   self.line:SetThickness(LINE_THICKNESS)
-  self.line:SetColorTexture(1, 1, 0, 1)
+  self.line:SetColorTexture(unpack(DEFAULT_LINE_COLOR))
 
   self.line:SetStartPoint("CENTER", "UIParent")
 end
@@ -64,10 +65,23 @@ function Addon:PLAYER_LOGIN()
   self.frame:UnregisterEvent("PLAYER_LOGIN")
 end
 
+function Addon:ColorLine()
+  if (UnitIsPlayer(UNIT_TARGET)) then
+    local color = RAID_CLASS_COLORS[select(2, UnitClass(UNIT_TARGET))]
+    self.line:SetColorTexture(color.r, color.g, color.b, 1)
+  else
+    self.line:SetColorTexture(unpack(DEFAULT_LINE_COLOR))
+  end
+end
+
 function Addon:UpdateLine()
-  local nameplate = GetNamePlateForUnit(UNIT_TARGET, issecure())
-  self:AnchorLine(nameplate)
-  self:ToggleLine(nameplate ~= nil)
+  local namePlate = GetNamePlateForUnit(UNIT_TARGET, issecure())
+
+  self:AnchorLine(namePlate)
+  self:ColorLine()
+  self:ToggleLine(namePlate ~= nil)
+
+  self.namePlate = namePlate
 end
 
 function Addon:PLAYER_TARGET_CHANGED()
@@ -79,7 +93,7 @@ function Addon:NAME_PLATE_UNIT_ADDED()
 end
 
 function Addon:NAME_PLATE_UNIT_REMOVED()
-  if (self:GetLastUnit() == nil) then
+  if (self:GetNamePlateUnit() == nil) then
     self:HideLine()
   end
 end
